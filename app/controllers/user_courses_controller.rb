ce68246dc8728c399b52c5e3c6583a8f393bcdef
Nguyen Huy <huynd6793@gmail.com>
2016-10-19 17:21:41 +0700

    detail course

--- app/controllers/user_courses_controller.rb
+++ app/controllers/user_courses_controller.rb
@@ -1,4 +1,7 @@
 class UserCoursesController < ApplicationController
+  load_and_authorize_resource
+
+  before_action :load_course, only: :show
 
   def index
     type = params[:type]
@@ -8,5 +11,16 @@ class UserCoursesController < ApplicationController
     else
       @user_courses = get_user_courses.status_of_course Course.statuses[type]
     end
+  end
+
+  def show
+    @subjects = @course.subjects
+    @trainer_in_course = @course.users.page params[:page]
+      .per Settings.pagination.per_page_member
+  end
+
+  private
+  def load_course
+    @course = @user_course.course
   end
 end
